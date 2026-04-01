6dda068ab22cf70aefa32e4a6e72e0aec628a39a
oliwia <24637207+kkanden@users.noreply.github.com>
2026-04-01 21:36:04 +0200

    runlength: rewrite idk i dont think im good at any of this i feel dumb writing c

--- runlength/main.c
+++ runlength/main.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 
 typedef int8_t i8;
 typedef int16_t i16;
@@ -13,11 +13,6 @@ typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-
-typedef struct {
-    char *str;
-    u64 size;
-} string;
 
 #define NDIGITS(n)                                                             \
     ({                                                                         \
@@ -30,98 +25,36 @@ typedef struct {
         len;                                                                   \
     })
 
-#define IS_NUM(c) (((c) >= 48 && (c) <= 57) ? true : false)
-#define IS_ALPHA(c)                                                            \
-    ((((c) >= 32 && (c) <= 47) || ((c) >= 58 && (c) <= 126)) ? true : false)
+//
 
-string rle(string data) {
-    u64 capacity = data.size * 2;
-    string encoded = {0};
-    encoded.str = malloc(capacity);
-    char current = data.str[0];
-    if (!IS_ALPHA(current)) {
-        fprintf(stderr, "Invalid character at position 0: %c\n", current);
-        exit(EXIT_FAILURE);
-    }
-    u64 offset = 0;
-    u64 count = 1;
-    for (u64 i = 1; i <= data.size; i++) {
-        if (i < data.size && !IS_ALPHA(data.str[i])) {
-            fprintf(stderr, "Invalid character at position %lu: %c\n", i,
-                    data.str[i]);
-            exit(EXIT_FAILURE);
-        }
-        if (current == (i < data.size ? data.str[i] : '\0')) {
+void rle(FILE *input) {
+    i32 ch = fgetc(input);
+    while (ch != EOF) {
+        int current = ch;
+        u8 count = 1;
+        while ((ch = fgetc(input)) == current && count < 255) {
             count++;
-            continue;
         }
-        u64 count_nchar = NDIGITS(count); // number of digits
-        while (offset + 1 + count_nchar >= capacity) {
-            capacity *= 2;
-            encoded.str = realloc(encoded.str, capacity);
-        }
-        offset += sprintf(encoded.str + offset, "%c%lu", current, count);
-        encoded.size = offset; // 1 for letter
-        current = data.str[i];
-        count = 1;
+        putchar(current);
+        putchar(count);
     }
-    encoded.str = realloc(encoded.str, encoded.size + 1);
-
-    return encoded;
 }
 
-string unrle(string data) {
-    u64 capacity = data.size * 10;
-    string decoded = {0};
-    decoded.str = malloc(capacity);
-    char current;
-    u64 offset = 0;
-    u64 count;
-    u64 count_cap = 3; // max 3 digits for start
-    char *count_c = malloc(count_cap);
-    u64 count_len = 0;
-    char letter = data.str[0];
-    for (u64 i = 1; i <= data.size; i++) {
-        current =
-            (i < data.size ? data.str[i] : '\0'); // null byte for final flush
-        if (IS_ALPHA(current) || current == '\0') {
-            char *endptr;
-            count_c[count_len] = '\0';
-            count = strtoull(count_c, &endptr, 10);
-            while (offset + count + 1 >= capacity) {
-                capacity *= 2;
-                decoded.str = realloc(decoded.str, capacity);
-            }
-            for (u64 ind = 0; ind < count; ind++) {
-                decoded.str[offset + ind] = letter;
-            }
-            offset += count;
-            count_len = 0;
-            letter = current;
-        } else if (IS_NUM(current)) {
-            count_len++;
-            while (count_len >= count_cap) {
-                count_cap *= 2;
-                count_c = realloc(count_c, count_cap);
-            }
-            count_c[count_len - 1] = current;
-        } else {
-            fprintf(stderr, "invalid character: %c", current);
-            exit(EXIT_FAILURE);
+void unrle(FILE *input) {
+    while (1) {
+        i32 current = fgetc(input);
+        if (current == EOF)
+            break;
+        i32 count = fgetc(input);
+        if (count == EOF) {
+            break;
         }
+        for (i32 i = 0; i < count; i++)
+            putchar(current);
     }
-    free(count_c);
-    decoded.size = offset;
-    decoded.str[offset] = '\0';
-
-    return decoded;
 }
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        fprintf(stderr, "No arguments provided\n");
-        exit(EXIT_FAILURE);
-    }
     bool decode = false;
     int opt;
     while ((opt = getopt(argc, argv, "d")) != -1) {
@@ -133,13 +66,23 @@ int main(int argc, char **argv) {
             exit(EXIT_FAILURE);
         }
     }
-    for (int i = 1 + (int)decode; i < argc; i++) {
-        string data;
-        data.str = argv[i];
-        data.size = strlen(data.str);
-        string out = decode ? unrle(data) : rle(data);
-        printf("%s\n", out.str);
-        free(out.str);
+
+    if (optind < argc) {
+        // inline argument
+        for (int i = optind; i < argc; i++) {
+            FILE *f = fopen(argv[i], "rb");
+            if (!f) {
+                perror(argv[i]);
+                exit(EXIT_FAILURE);
+            }
+            decode ? unrle(f) : rle(f);
+        }
+    } else if (!isatty(STDIN_FILENO)) {
+        // data piped through
+        decode ? unrle(stdin) : rle(stdin);
+    } else {
+        fprintf(stderr, "No input provided\n");
     }
+
     exit(EXIT_SUCCESS);
 }
